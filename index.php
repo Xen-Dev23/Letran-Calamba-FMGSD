73487a5dd96dc6b9013b50faf8424bed89c25cff
Xen <opulenciaandrei23@gmail.com>
2025-04-07 17:37:58 +0800

    update

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home EHS | Letran Calamba</title>
+    <title>EHS | Letran Calamba</title>
     <link rel="stylesheet" href="index.css">
     <link rel="icon" type="image/png" href="../assets/images/favicon.png">
     <link rel="icon" type="image/x-icon" href="https://www.letran-calamba.edu.ph/static/img-content/logoLetran.webp">
